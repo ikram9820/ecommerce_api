f393b3aa33df2a6de586fe683a90c6b6f8007d23
ikram9820 <ikram9820khan@gmail.com>
2022-05-28 01:21:13 -0700

    initail commit

---
+++ ecomm/urls.py
@@ -0,0 +1,20 @@
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+admin.site.site_header = 'Storefront Admin'
+admin.site.index_title = 'Admin'
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('store.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('__debug__/', include('debug_toolbar.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
